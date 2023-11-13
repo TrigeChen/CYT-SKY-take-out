4bb1282302053aa9df110087ffc4a809e8dce900
TrigeChen <shaonian15769@gamil.com>
2023-11-13 21:55:29 +0800

    苍穹外卖初始化

---
+++ src/main/java/com/cyt/cytskytakeout/CytSkyTakeOutApplication.java
@@ -0,0 +1,13 @@
+package com.cyt.cytskytakeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CytSkyTakeOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CytSkyTakeOutApplication.class, args);
+    }
+
+}
