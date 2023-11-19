9ffc54e53468ae603ddeb2e0247c47f3c4c02822
TrigeChen <shaonian15769@gamil.com>
2023-11-19 17:07:13 +0800

    新增菜品功能开发-初始化

---
+++ sky-server/src/main/java/com/cyt/server/DishService.java
@@ -0,0 +1,13 @@
+package com.cyt.server;
+
+/**
+ * ClassName: DishService
+ * Package: com.cyt.server
+ * Description:
+ *
+ * @Author CYT
+ * @Create 2023/11/19 17:02
+ * @Version 1.0
+ */
+public interface DishService {
+}
