5f927a7fd6d26bc0330fe4d1995cb65bf35b09e7
TrigeChen <shaonian15769@gamil.com>
2023-11-19 17:07:17 +0800

    新增菜品功能开发-初始化

---
+++ sky-server/src/main/java/com/cyt/server/impl/DishServiceImpl.java
@@ -0,0 +1,19 @@
+package com.cyt.server.impl;
+
+import com.cyt.server.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * ClassName: DishServiceImpl
+ * Package: com.cyt.server.impl
+ * Description:
+ *
+ * @Author CYT
+ * @Create 2023/11/19 17:03
+ * @Version 1.0
+ */
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+}
